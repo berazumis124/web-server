4341398f6be9bcf7c6b3ba1ec67a3f44e5f2196d
berazumis124 <berazumis@gmail.com>
2017-04-04 19:59:04 +0300

    Changes in text

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get ('/yeah', function (req, res){
-    res.send('Yeah!!! It\'s running');
+    res.send('Yeah!!! It\'s running!');
 });
 
 app.listen(4998);
