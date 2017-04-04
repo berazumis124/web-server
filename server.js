e8bc81a8be5a9426c67e58441de088ee3e3240c0
berazumis124 <berazumis@gmail.com>
2017-04-04 20:43:25 +0300

    Update for heroku

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var app = express();
+var PORt = process.env.PORT || 4998;
 
 app.get ('/yeah', function (req, res){
     res.send('Yeah!!! It\'s running!');
 });
 
-app.listen(4998);
+app.listen(PORT);
